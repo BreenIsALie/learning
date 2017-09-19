93e179726cd9ca17b02a3023ceda759e15891dab
BreenIsALie <mathias.c.jonsson@ericsson.com>
2017-09-19 12:40:36 +0200

    exercise 1
    Full file commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# enviroment test
+# git commit test
+
+print "hello world"
+print "hello again"
+print "i like typing this"
+print "this is fun"
+print "yay! printing"
+print "i'd much rather you 'not'"
+print 'id "said" do not touch this'
